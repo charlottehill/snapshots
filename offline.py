a283ec559c3013b31227862b42e0b595c19d97f1
Charlotte Hill <charlotte@hillnetwork.com>
2014-02-26 10:13:20 -0800

    Add index.html; separate and single page results

--- offline.py
+++ offline.py
@@ -28,7 +28,7 @@ def html_view(title):
     html.append('\n<h1 style="text-align:center">{0}</h1>'.format(title.title()))
     html.append('<hr>')
     # include stylesheet
-    html.append('<link rel="stylesheet" type="text/css" href="style.css" />')
+    html.append('<link rel="stylesheet" type="text/css" href="http://octopi.cs.ucsb.edu/analysis/stylesheets/script_style.css" />')
     #<!-- Include jQuery -->
     html.append('\n<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/'
                 'jquery.min.js"></script>')
@@ -72,50 +72,52 @@ def main():
 
     # get the directory from the path
     dirname = os.path.basename(os.path.normpath(path))
+    if not os.path.exists('{0}/{1}results'.format(target, dirname)):
+        os.makedirs('{0}/{1}results'.format(target, dirname))
 
-    # do you want the results on one page or on different pages for each file?
-    single_file = True
+    for plugin_class in modules[module]:
+        # returns a dictionary of html strings for each file
+        single = [html_view(module)]
+        # build index.html 
+        index =['<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" ']
+        index.append('"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">')
+        index.append('\n<html lang="en" xml:lang="en" xmlns="http://www.w3.org/1999/xhtml">')
+        index.append('\n<!-- InstanceBegin template="/Templates/Template.dwt.php" codeOutsideHTMLILocked="false" -->')
+        index.append('\n<head>\n<link href="http://octopi.cs.ucsb.edu/analysis/stylesheets/style.css" rel="stylesheet" type="text/css" />')
+        index.append('\n<meta http-equiv="Content-Type" content="text/html; charset=iso-8859-1" />')
+        index.append('\n<meta name="language" content="en" />')
+        index.append('\n')
+        plugin = plugin_class()
+        name = plugin.__class__.__name__
+        index.append('\n<title>{0}</title>'.format(dirname))
+        index.append('\n</head>\n<body>\n<div class="content">')
+        index.append('\n<h1>{0} {1}</h1>'.format(dirname, name))
+        results = plugin._process(path)
+        # returns a dictionary of html strings for each file
+        html_results = htmlwrappers[name](results)
+        for file in sorted(html_results.iterkeys()):
+            index.append('\n<br><a href="./{0}_{1}.html">{0}</a>'.format(file, module))
+            # html for this file for this module
+            single.append(html_results[file])
+            # html for multiple files
+            multiple = [html_view(module)]
+            multiple.append(html_results[file])
+            multiple.append('</body>')
+            multiple.append('</html>')
+            # write the results for this file
+            with open('{0}/{1}results/{2}_{3}.html'.format(
+                    target, dirname, file, module), 'w') as fp:
+                fp.write(''.join(multiple))
+        # add on the closing html
+        single.append('</body>')
+        single.append('</html>')
+        # write the results for all the files
+        with open('{0}/{1}results/{1}_{2}.html'.format(target, dirname, module), 'w') as fp:
+            fp.write(''.join(single))
+        index.append('\n<br><br><a href="./{0}_{1}.html">View all on one page</a>'.format(dirname,module))
+        with open('{0}/{1}results/index.html'.format(target, dirname), 'w') as fp:
+            fp.write(''.join(index))
 
-    if single_file:
-        # add the headers and stuff to the page
-        html_list = [html_view(module)]
-        for plugin_class in modules[module]:
-            plugin = plugin_class()
-            name = plugin.__class__.__name__
-            results = plugin._process(path)
-            # returns a dictionary of html strings for each file
-            html_results = htmlwrappers[name](results)
-            for file in sorted(html_results.iterkeys()):
-                # html for this file for this module
-                html_list.append(html_results[file])
-            # add on the closing html
-            html_list.append('</body>')
-            html_list.append('</html>')
-            # write the results for all the files 
-            with open('{0}/{1}_{2}.html'.format(target, dirname, module), 'w') as fp:
-                fp.write(''.join(html_list))
-    else:
-        for plugin_class in modules[module]:
-            # make a directory for all the results pages
-            if not os.path.exists('{0}results'.format(dirname)):
-                os.makedirs('{0}results'.format(dirname))
-            plugin = plugin_class()
-            name = plugin.__class__.__name__
-            results = plugin._process(path)
-            # returns a dictionary of html strings for each file
-            html_results = htmlwrappers[name](results)
-            for file in sorted(html_results.iterkeys()):
-                # add the headers and stuff to the page
-                html_list = [html_view(module)]
-                # html for this file for this module
-                html_list.append(html_results[file])
-                # add on the closing html
-                html_list.append('</body>')
-                html_list.append('</html>')
-                # write the results for this file
-                with open('{0}/{1}results/{2}_{3}.html'.format(
-                        target, dirname, file, module), 'w') as fp:
-                    fp.write(''.join(html_list))
 
 if __name__ == '__main__':
     sys.exit(main())
