a1e212ccac42b73307274b7914da0c270e7e63ff
Charlotte Hill <charlotte@hillnetwork.com>
2014-02-28 10:05:24 -0800

    Add comments

--- scripts.py
+++ scripts.py
@@ -15,6 +15,7 @@ class Scripts(SnapshotPlugin):
 
     def __init__(self):
         super(Scripts, self).__init__()
+        # add a name and description that we can put on the website
         self._modulename = "Scripts"
         self._description = "Show the scripts for each file."
 
@@ -26,34 +27,51 @@ class Scripts(SnapshotPlugin):
             self.script_count[sprite.name] = 0
             for script in sprite.scripts:
                 if not isinstance(script, kurt.Comment):
+                    # update the script count
                     self.script_count[sprite.name] += 1
                     scripts[sprite.name].add(script)
         return scripts
 
+
     def analyze(self, path):
         # keep a count of the number of visible scripts for each sprite
         # use this to order them at the end
-        # any with zero scripts will be listed separately
+        # any sprites with zero scripts will be listed separately
         self.script_count = Counter()
+
+        # dictionary of all the scripts
+        # scripts[filename] = sprites
+        # scripts[filename][spritename] = set of scripts
         scripts = {}
+
+        # get the directory name
         dirname = os.path.dirname(path)
-        for file in os.listdir(path):
+        # go through oct files
+       for file in os.listdir(path):
             if file.endswith(".oct"):
                 # get file name
                 base = os.path.basename(file)
                 filename = os.path.splitext(base)[0]
+
                 # set up kurt project
                 oct = kurt.Project.load(os.path.abspath(os.path.join(path, file)))
+
+                # get scripts for that file
                 scripts[filename] = self.get_scripts(oct)
+       #Counter - used to order the sprites on the webpage
+       #Scripts - the scripts for each sprite for each file
         return {'counter': self.script_count, 'scripts': scripts}
 
 
-# Displays sprite names and pictures
+# Displays the scripts for each sprite for each file
 def scripts_display(results):
     files = results['scripts']
     script_count = results['counter']
+
+    # this will store the sprites that don't have any visible scripts
     other = []
-    # return a dictionary of html strings for each file
+
+    # we will return a dictionary of html strings for each file
     output = {}
 
     # make a list of sprites without scripts
@@ -65,26 +83,46 @@ def scripts_display(results):
     for filename, scriptdict in files.items():
         # main = text that goes across the page
         html = ['\n<div class="main">']
+
+        # add a heading
         html.append('<h2>Scripts for {0}</h2><br></div>'.format(filename))
-        # only go through the sprites with scripts
-        # go from most to least amount of scripts
+
+        # we'll only go through the sprites with scripts
         num = len(script_count.keys()) - len(other)
+
+        # go from most to least amount of scripts 
         for sprite, count in script_count.most_common(num):
-            # col = page is split into four equal columns
+            # col = content is split into equal sized columns
+            # each sprite will have a column of scripts
             html.append('\n<div class="col">')
-            # for each sprite, print all its scripts in a column
+
+            # print the name of the sprite
+            # to do?: add a thumbnail
             html.append(sprite)
+
+            # this is what scratchblocks uses to make the blocks look pretty
             html.append('<pre class="blocks">')
+
+            # make sure this sprite is in this file
             if sprite in scriptdict.keys():
+                # get the sprite's scripts
                 for script in scriptdict[sprite]:
+                    # to scratch blocks prints the script in the
+                    # format needed by scratchblocks
                     html.append(SnapshotPlugin.to_scratch_blocks(sprite, script))
             else:
+                # this sprite isn't in this file, just print an empty line
                 html.append('<br>')
+            # close the blocks pre class
             html.append('</pre>')
+            # close the column div class
             html.append('</div>')
 
         # List the sprites without scripts
+        # to do?: just print this once at the bottom of the page
         html.append('\n<div class="main">')
+        # lots of fancy footwork to get the and in the right place
+        # change this if there's a nicer way to do it
         if len(other) != 0:
             html.append('\n<br>')
             html.append('Sprites that don\'t ever have visible scripts: ')
@@ -95,5 +133,8 @@ def scripts_display(results):
                     html.append('{}, '.format(other[n]))
                 html.append('and {}.'.format(other[-1]))
         html.append('<br><br><br>')
+
+        # add the html (as a string) to the dictionary
         output[filename] = ''.join(html)
+
     return output
